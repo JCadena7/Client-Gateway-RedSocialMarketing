9ac2b0b663c9ae6e248d1c2556577baa765314a1
JCadena7 <lorenzofastidio176@gmail.com>
2025-11-16 21:06:29 -0500

    Sign-out and refresh

--- src/auths/auths.controller.ts
+++ src/auths/auths.controller.ts
@@ -8,8 +8,11 @@ import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { SupabaseAuthGuard } from '../auth/guards/supabase-auth.guard';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
+import { SignOutDto } from './dto/sign-out.dto';
 
 @Controller('auths')
+// @UseGuards(SupabaseAuthGuard)
 export class AuthsController {
   constructor(@Inject(AUTH_SERVICE) private readonly authsService: ClientProxy) {}
 
@@ -29,10 +32,31 @@ export class AuthsController {
     return this.authsService.send('auth.validateEmail', dto);
   }
 
+  @Post('refresh')
+  refresh(@Body() dto: RefreshTokenDto) {
+    return this.authsService.send('auth.refresh', dto);
+  }
+
   @UseGuards(SupabaseAuthGuard)
   @Post('usuarios')
   createUsuario(@Body() dto: CreateUsuarioDto) {
     return this.authsService.send('usuarios.create', dto);
+  }
+
+  @UseGuards(SupabaseAuthGuard)
+  @Post('sign-out')
+  signOut(@Body() dto: SignOutDto) {
+    console.log('🔵 Gateway - signOut:', dto);
+  //   try {
+  //     return this.authsService.send('auth.signOut', dto);
+  //   } catch (error) {
+  //     console.error('🔴 Gateway - Error al cerrar sesión:', error);
+  //     throw error;
+  //   }
+  // }
+    const response = this.authsService.send('auth.signOut', dto);
+    console.log('🔵 Gateway - signOut response:', response);
+    return response;
   }
 
   // CRUD endpoints
